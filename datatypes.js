dd924e191bce0570f2b93739c6cad2cb71ec436a
MUHAMMAD MUBAARAK <126537753+baarak08@users.noreply.github.com>
2023-02-28 21:08:13 +0700

    exercise1

---
+++ datatypes.js
@@ -0,0 +1,12 @@
+let num1 = 10
+let num2 = 30
+
+
+let msg = "Hello"
+let space = " "
+let msg2 = "World" 
+let msg3 = msg + space + msg2
+
+console.log(msg3);
+
+console.log(Boolean(10));
---
+++ exercise2.js
@@ -0,0 +1,19 @@
+1 // Convert Celcius to Fahrenheit
+for (let c = 0; c <= 5; c++) {
+    let F = (c * 1.8) + 32
+    
+console.log(F);
+
+}
+
+2 // Menentukan Gaanjil Genap
+for (let num = 1; num <= 5; num++) {
+    if (num % 2 === 0) {
+        console.log("Genap");
+    } else {
+        console.log("Ganjil");
+    }
+
+}
+
+3 // 
